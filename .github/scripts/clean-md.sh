4348552eb52eb78937fcdbdef086aac42264903a
kimmking <kimmking@apache.org>
2020-08-07 22:27:04 +0800

    add action

---
+++ .github/scripts/clean-md.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/apache/shardingsphere
+ll shardingsphere/docs/document/content/
+cp -rf shardingsphere/docs/document/content/* md 
+rm -rf shardingsphere
+
+
+cd md
+
+find . -name '*.en.md' | xargs rm -rf 
+find . -name '*.cn.md' | xargs sed -i '1d' 
+find . -name '*.cn.md' | xargs sed -i -n '/+++/,$ p' 
+find . -name '*.cn.md' | xargs sed -i '1d' 
+
+gitbook pdf
+
+cd ..
+
+git add .
+git commit -m "update"
+git push
